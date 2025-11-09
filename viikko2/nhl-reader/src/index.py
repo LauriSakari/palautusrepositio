930b51f2f3b40d20743cb32a18b12930fe0deafb
LauriSakari <lauri.harjukoski@gmail.com>
2025-11-09 21:35:34 +0200

    tehtävä 5 tehty

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -2,17 +2,40 @@
 from player_reader import PlayerReader
 from player_stats import PlayerStats
 
+from rich.table import Table
+from rich.console import Console
+
+
 def main():
 
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-
+    season = input("Season [2018-19, 2019-20, 2020-21, 2021-22, 2022-23, 2023-24, 2024-25, 2025-26]: ")
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+        
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+    nationalities = stats.get_nationalities()
+    
+    while True: 
+        nationality = input(f"Nationality [{nationalities}]").upper()
+        players = stats.top_scorers_by_nationality(nationality)
 
 
-    for player in players:
-        print(player)
+
+        console = Console()
+        table = Table(title="Suomalaiset huippupelaajat")
+
+        table.add_column("Nimi", style="cyan")
+        table.add_column("Joukkue")
+        table.add_column("Maalit", justify="right")
+        table.add_column("Syötöt", justify="right")
+        table.add_column("Pisteet", justify="right")
+
+
+
+        for p in players:
+            table.add_row(p.name, p.team, str(p.goals), str(p.assists), str(p.goals + p.assists))
+
+        console.print(table)
         
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player_stats.py
+++ viikko2/nhl-reader/src/player_stats.py
@@ -3,6 +3,14 @@ class PlayerStats:
     def __init__(self, data):
         self.data = data
 
+    def get_nationalities(self):
+        nationalities = set()
+        for player in self.data.players:
+            nationalities.add(player.nationality)
+        nationalities_str = ", ".join(sorted(nationalities))
+        return nationalities_str
+            
+
     def top_scorers_by_nationality(self, nationality):
         players_sorted = sorted(self.data.players, key=lambda p: p.goals + p.assists, reverse=True)
 
